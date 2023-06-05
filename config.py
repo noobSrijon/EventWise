41056c7b1c8063d254fd0118c1b0c3b543b9e296
Srijon kumar <37466825+noobSrijon@users.noreply.github.com>
2023-06-06 02:35:11 +0600

    First Commit

---
+++ config.py
@@ -0,0 +1,4 @@
+admin_username = "admin"
+admin_password = "password"
+imgur_client_id = "your_client_id"
+sms_api_key= "sms_api_key" #You may change request url on config() with your provider's api url
